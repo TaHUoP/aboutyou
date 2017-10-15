1547a1e6078d5445794b2dfc994972809a82e3ab
TaHUoP <malhasianv@gmail.com>
2017-10-15 16:40:35 +0300

    FIX: BaseEntity getter and setter

--- src/AboutYou/Entities/BaseEntity.php
+++ src/AboutYou/Entities/BaseEntity.php
@@ -8,15 +8,23 @@ abstract class BaseEntity
     /**
      * @param string $key
      * @param mixed $value
+     * @return boolean
      */
     public function __set($key, $value) {
-        $this->{'set' . $key}($value);
+        if(method_exists(get_called_class(), 'set' . $key)){
+            $this->{'set' . $key}($value);
+            return true;
+        }else{
+            return false;
+        }
     }
 
     /**
      * @param string $key
+     * @return mixed
      */
     public function __get($key) {
-        return $this->{'get' . $key};
+        return method_exists(get_called_class(), 'get' . $key) ?
+            $this->{'get' . $key}() : null;
     }
 }
